521491382305b817dd19b010d6c295c5c3e610c9
zasxdc3f <152961977+zasxdc3f@users.noreply.github.com>
2025-04-26 23:42:17 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -5,21 +5,21 @@ eval set -- "$OPTS"
 while [ $# -gt 0 ]
 do
   case "$1" in
-    --max_depth) MAX_DEPTH="$2"; shift; break;;
+    --max_depth) MAX_DEPTH="$2"; shift 2; break;;
     *) break
   esac
 done
 
-INPUT_DIR="$2"
-OUTPUT_DIR="$3"
+INPUT_DIR="$1"
+OUTPUT_DIR="$2"
 
 if [ -n "$MAX_DEPTH" ]; then
   PATHS=$(find "$INPUT_DIR" -maxdepth "$MAX_DEPTH" -type f)
 else
   PATHS=$(find "$INPUT_DIR" -type f)
 fi
-
 for FILE in $PATHS
+
 do
   if [ -f "$OUTPUT_DIR/$(basename "$FILE")" ]; then
     cp "$FILE" "$OUTPUT_DIR/$(basename "$FILE")_"
