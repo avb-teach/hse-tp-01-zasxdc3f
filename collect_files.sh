390a1ba5f640c735eff01469195b85405039ba9f
zasxdc3f <152961977+zasxdc3f@users.noreply.github.com>
2025-04-26 23:14:24 +0300

    added collect_files.sh

---
+++ collect_files.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+OPTS=$(getopt -l "max_depth:" -- -- "$@")
+eval set -- "$OPTS"
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    --max_depth) MAX_DEPTH="$2"; shift; break;;
+    *) break
+  esac
+done
+
+INPUT_DIR="$2"
+OUTPUT_DIR="$3"
+
+if [ -n "$MAX_DEPTH" ]; then
+  PATHS=$(find "$INPUT_DIR" -maxdepth "$MAX_DEPTH" -type f)
+else
+  PATHS=$(find "$INPUT_DIR" -type -f)
+fi
+
+for FILE in $PATHS
+do
+  if [ -f "$OUTPUT_DIR/$(basename "$FILE")" ]; then
+    cp "$FILE" "$OUTPUT_DIR/$(basename "$FILE")_"
+  else
+    cp "$FILE" "$OUTPUT_DIR/"
+  fi
+done
